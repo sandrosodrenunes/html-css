733b01ba8d44fe5b31035930d4e37ce0c83741d7
Sandro Sodré Nunes <sandrojunior802@gmail.com>
2023-09-24 11:47:55 -0300

    exercício 26 de mídia query

---
+++ exercicios/ex026/mq005/app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#menu')
+
+function clickMenu() {
+
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none'
+    } else{
+        menu.style.display = 'block'
+    }
+    
+}
